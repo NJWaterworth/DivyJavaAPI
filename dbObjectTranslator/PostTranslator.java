e0d24f40b61e438246f8e1042a41f41265407be5
Jkharrison <jasperharison@gmail.com>
2018-11-02 15:35:26 -0500

    Testing needs to be done with Dozer to make sure this works.

--- dbObjectTranslator/PostTranslator.java
+++ dbObjectTranslator/PostTranslator.java
@@ -2,12 +2,12 @@ package dbObjectTranslator;
 
 import constants.AttributeNames;
 import dbObjectModel.Post;
-
+import org.dozer.DozerBeanMapper;
 public class PostTranslator implements DDBTranslator {
 
     public Post dynamoToInternal(Item item) {
-        Post post = new Post();
-        post.caption = item.getString(AttributeNames.CAPTION);
+        // Post post = new Post();
+        Post post = new DozerBeanMapper().map(item, Post.class);
         // etc
         // I think theres probably a more generic way to do this but I'm tired.
         // I'll keep thinking about it
@@ -17,6 +17,7 @@ public class PostTranslator implements DDBTranslator {
 
     public Item internalToDynamo(Post post) {
         // Do this too
+        Item item = new DozerBeanMapper().map(post, Item.class);
         return null;
     }
 }
